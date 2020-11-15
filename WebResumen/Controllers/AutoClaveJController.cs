a2301b088e8a88cb3ca9cda366a46a29f6e38a5f
fuentei3 <fuentei3@ESSA0538NA>
2020-11-15 22:40:33 +0100

    Se añade politica de privacidad-logo -y querys basicas

--- WebResumen/Controllers/AutoClaveJController.cs
+++ WebResumen/Controllers/AutoClaveJController.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebResumen.Models;
 
 namespace WebResumen.Controllers
 {
+    [Authorize(Policy = "ADRoleOnly")]
     public class AutoClaveJController : Controller
     {
 
